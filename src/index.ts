91075cc86b28aa6405920230ccea351f553f7c8c
Federico <tacchinifederico@gmail.com>
2018-02-04 19:00:05 +0100

    Exporting status codes

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ Types.HttpControllerActivator = Symbol("HttpControllerActivator");
 export * from "./server";
 export * from "./routed-controller";
 export * from "./http-route-type";
+
+import * as HttpStatus from 'http-status-codes'
+export const HttpStatusCodes = HttpStatus;
