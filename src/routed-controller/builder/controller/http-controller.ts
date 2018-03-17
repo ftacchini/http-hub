fe6da5c1772271b7b5f58a652fce0c0d17bb38aa
Federico <tacchinifederico@gmail.com>
2018-03-17 19:00:13 +0100

    injectable in http controller

--- src/routed-controller/builder/controller/http-controller.ts
+++ src/routed-controller/builder/controller/http-controller.ts
@@ -1,7 +1,9 @@
 import { Server } from "ts-hub";
 import { HttpServer } from "../../../server/http-server";
 import { AbstractHttpControllerBuilder } from "./abstract-http-controller-builder";
+import { injectable } from "inversify";
 
+@injectable()
 export class HttpController extends AbstractHttpControllerBuilder {
 
     public supportsServer(server: Server) : boolean {
--- src/routed-controller/builder/controller/https-controller.ts
+++ src/routed-controller/builder/controller/https-controller.ts
@@ -1,7 +1,9 @@
 import { Server } from "ts-hub";
 import { HttpsServer } from "../../../server/https-server";
 import { AbstractHttpControllerBuilder } from "./abstract-http-controller-builder";
+import { injectable } from "inversify";
 
+@injectable()
 export class HttpsController extends AbstractHttpControllerBuilder {
 
     public supportsServer(server: Server) : boolean {
