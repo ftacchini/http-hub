488b7c581547587d84efbef0652ac95b14be4bf3
Federico <tacchinifederico@gmail.com>
2018-05-05 20:53:02 +0200

    Updating to ts-hub 3.1

--- src/routed-controller/middleware/http-activator-middleware.ts
+++ src/routed-controller/middleware/http-activator-middleware.ts
@@ -1,10 +1,12 @@
 import { RequestHandler } from 'express';
 import { Middleware } from 'ts-hub';
 import { ErrorRequestHandler } from 'express-serve-static-core';
+import { ExecutionOrder } from 'ts-hub/dts/src/routed-controller/execution-order';
 
 export class HttpActivatorMiddleware<HttpRequestHandler extends RequestHandler | ErrorRequestHandler> implements Middleware<null, HttpRequestHandler> {
     public readonly priority: number = 0;
     public readonly information: null = null;
+    public readonly executionOrder: ExecutionOrder;
 
     constructor(private requestHandler: HttpRequestHandler) {
 
